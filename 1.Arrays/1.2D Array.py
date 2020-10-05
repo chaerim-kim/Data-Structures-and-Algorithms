48bd31d4409d308e528ed2a17087195d2c879980
Chae Rim Kim <sc17crk@leeds.ac.uk>
2020-10-05 19:46:30 +0900

    Added folders and completed 1. array

---
+++ 1.Arrays/1.2D Array.py
@@ -0,0 +1,29 @@
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    val = 0
+    val_list=[]
+
+    # i = row, j = col
+    for i in range (len(arr)-2):        # 4 reps
+        for j in range (len(arr)-2):        # 4 reps
+            # calculate the hourglass
+            val = arr[i][j] + arr[i][j+1] + arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+            # add all hourglass sum values to a list
+            val_list.append(val)
+
+    return max(val_list)
+
+
+
+
+if __name__ == "__main__":
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
